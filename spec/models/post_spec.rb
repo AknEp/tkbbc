1c1bc651c765edb1e323a00074aece3cd334df91
AknEp <akn@s-satoshi.net>
2013-11-25 20:24:45 +0900

    update のテストを追記した

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,9 @@
 require 'spec_helper'
 
 describe Post do
-  describe "#create" do
-    let(:too_long_string) {'a'*(Post::BODY_MAX_LEN + 1)}
+  let(:too_long_string) {'a'*(Post::BODY_MAX_LEN + 1)}
 
+  describe "#create" do
     it "should have body text" do
       Post.create(body: 'hoge')
       expect(Post.find(1).body.present?).to be_true
@@ -15,5 +15,28 @@ describe Post do
       @post = Post.new(body: too_long_string)
       expect(@post).not_to be_valid
     end
+  end
+
+  describe "#update" do
+    before(:each) do
+      @post = Post.create(body: 'hoge')
+    end
+
+    it "should body equals to be fuga" do
+      @post.body = 'fuga'
+      @post.save
+
+      expect(Post.find(1).body).to eq('fuga')
+    end
+
+    it "should not body equals to be fuga" do
+      expect(Post.find(1).body).not_to eq('fuga')
+    end
+
+    it "should not be valid with too long body(more than #{Post::BODY_MAX_LEN} characters)" do
+      @post.body = too_long_string
+      expect(@post).not_to be_valid
+    end
+
   end
 end
