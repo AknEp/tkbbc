57c2a36f561d781de0c3211fe853d6ada504607c
AknEp <akn@s-satoshi.net>
2013-12-16 23:22:02 +0900

    shouldじゃなくexpectで書くように変更しました

--- spec/views/boards/edit.html.slim_spec.rb
+++ spec/views/boards/edit.html.slim_spec.rb
@@ -14,11 +14,11 @@ describe "boards/edit" do
 
   it "form exists" do
     page.find("form[action='#{board_path(@board)}'][method=post]" ) do |form|
-      form.should have_selector("input[name='board[title]']", count: 1) do |title_input|
-        title_input.first["value"].should eq(@board.title)
-        form.should have_selector("label[for='#{title_input.first["id"]}']", count: 1)
+      expect(form).have_selector("input[name='board[title]']", count: 1) do |title_input|
+        expect(title_input.first["value"]).eq(@board.title)
+        expect(form).have_selector("label[for='#{title_input.first["id"]}']", count: 1)
       end
-      form.should have_selector("input[type=submit]")
+      expect(form).have_selector("input[type=submit]")
     end
   end
 
