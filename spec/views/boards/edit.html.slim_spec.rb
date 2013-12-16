edadc2bd316afc4baa5f87abbba57c421e49f658
AknEp <akn@s-satoshi.net>
2013-12-16 21:04:46 +0900

    edit のテストコードを追記、Labelも追加

--- spec/views/boards/edit.html.slim_spec.rb
+++ spec/views/boards/edit.html.slim_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
 describe "boards/edit" do
+
+  let(:title){ "some title" }
+
   before(:each) do
-    @board = assign(:board, stub_model(Board))
+    @board = assign(:board, stub_model(Board, title: title ))
   end
 
   it "renders the edit board form" do
     render
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", board_path(@board), "post" do
+    assert_select "form[action=?][method=?]", board_path(@board), "post" do |form_elements|
+      assert_select "input[name=?][value=?]", "board[title]", @board.title do |elements|
+        elements.each do |element|
+          form_elements.each do |form_element|
+            assert_select form_element, "label[for=?]", element.attributes["id"]
+          end
+        end
+      end
+      assert_select "input[type=?]", :submit
+      #assert_select "label[for=?]", "board_title"
     end
   end
 end
